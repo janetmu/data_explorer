da258ac440aa1f141e56abe4227638e7677b7ce2
Janet Marulanda <janet_marulanda@yahoo.com>
2023-11-20 01:31:50 -0500

    first commit

---
+++ backend/src/test/java/com/data_explorer/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_explorer.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
